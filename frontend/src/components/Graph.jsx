40acf743100e9b790ce88b444f6bb1b491c3f29e
Cesar Tejaxun <ct@fibbo.app>
2021-06-24 23:39:53 -0600

    grafica - actulizacion de  paremtros para componente tabla y traslado de funciones de ayuda a generadores.js

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -1,32 +1,20 @@
 import { useState } from "react";
 import { useEffect, useRef } from "react"
-import { get_months, parse_date } from "../helpers/Generadores";
+import { get_labels } from "../helpers/Generadores";
 
-import { MESES } from "../helpers/Variables"
 export default function Graph({ from = "2020-05", to = "2020-05", data = [] }) {
 
     const canvas_el = useRef(null)
     const [chart, set_chart] = useState(null)
     const get_data = () => {
-        const from_date = parse_date(from);
-        const to_date = parse_date(to);
-        const labels = get_months(from, to).map(item => `${item.year} - ${MESES[item.month]}`);
-        const datasets = data.map(item => {
-            const fechas = item.fecha;
-            const new_data = item.data.map((jtem, jndex) => {
-                const d = fechas[jndex];
-                return d >= from_date && d <= to_date ? jtem : 0;
-            });
-            return { ...item, data: new_data }
-        });
         return {
-            labels,
-            datasets
+            labels: get_labels(from, to),
+            datasets: data
         };
     }
     const get_config = () => {
         const data = get_data()
-        console.log(data);
+        // console.log(data);
         return {
             type: 'line',
             data,
--- frontend/src/helpers/Generadores.js
+++ frontend/src/helpers/Generadores.js
@@ -1,4 +1,6 @@
 
+import { MESES } from "./Variables"
+
 const random = (length, from) => Math.floor(Math.random() * length + from)
 const numbers = (from, to) => {
     const l = []
@@ -54,6 +56,41 @@ const get_months = (from, to) => {
     return months;
 }
 
+const get_dataset_from_data = (from, to, ...data) => {
+    const from_date = parse_date(from);
+    const to_date = parse_date(to);
+    return data.map(item => {
+        const fechas = item.fecha;
+        const new_data = item.data.map((jtem, jndex) => {
+            const d = fechas[jndex];
+            return d >= from_date && d <= to_date ? jtem : 0;
+        });
+        return { ...item, data: new_data }
+    });
+}
+
+const get_labels = (from, to) => {
+    return get_months(from, to).map(item => `${item.year} - ${MESES[item.month]}`);
+}
+
+const group_by = (key, list) => {
+    const tt = {}
+    list.forEach((item, index) => {
+        var jtem = tt[item[key]];
+        if (!jtem) {
+            jtem = { list: [] }
+            tt[item[key]] = jtem;
+        }
+        jtem["list"].push(item)
+    });
+    return tt;
+}
+
 export {
-    generate_data, random, numbers, get_diff_in_months, get_months, parse_date,
+    generate_data, random, numbers,
+    get_diff_in_months,
+    get_months, parse_date,
+    get_dataset_from_data,
+    get_labels,
+    group_by,
 }
