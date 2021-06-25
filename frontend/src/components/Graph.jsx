37604bc4c28c729553af0ddeb08ca024316f9a25
Cesar Tejaxun <ct@fibbo.app>
2021-06-24 22:45:25 -0600

    grafica -  procesa de  obtencion de servicios getRaking para construir grafica

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
 import { useEffect, useRef } from "react"
+import { get_months, parse_date } from "../helpers/Generadores";
 
 import { MESES } from "../helpers/Variables"
-export default function Graph({ data }) {
+export default function Graph({ from = "2020-05", to = "2020-05", data = [] }) {
 
-    const { from = 0, to = 11, bancos: datasets = [] } = data
     const canvas_el = useRef(null)
-
+    const [chart, set_chart] = useState(null)
     const get_data = () => {
-        const labels = MESES.filter((item, index) => index >= from && index <= to);
+        const from_date = parse_date(from);
+        const to_date = parse_date(to);
+        const labels = get_months(from, to).map(item => `${item.year} - ${MESES[item.month]}`);
+        const datasets = data.map(item => {
+            const fechas = item.fecha;
+            const new_data = item.data.map((jtem, jndex) => {
+                const d = fechas[jndex];
+                return d >= from_date && d <= to_date ? jtem : 0;
+            });
+            return { ...item, data: new_data }
+        });
         return {
             labels,
             datasets
         };
     }
-
     const get_config = () => {
+        const data = get_data()
+        console.log(data);
         return {
             type: 'line',
-            data: get_data(),
+            data,
             options: {
                 scales: {
                     y: {
@@ -41,9 +53,12 @@ export default function Graph({ data }) {
     }
 
     useEffect(() => {
-        console.log(data);
-        new window.Chart(canvas_el.current, get_config())
-    }, [])
+        if (chart) {
+            chart.destroy();
+        }
+        set_chart(new window.Chart(canvas_el.current, get_config()))
+    }, [data])
+
 
     return (
         <div className="graph">
--- frontend/src/helpers/Generadores.js
+++ frontend/src/helpers/Generadores.js
@@ -18,7 +18,42 @@ const generate_data = (from = 0, to = 11, ...names) => {
 }
 
 
+const parse_date = str => {
+    const [year, month] = str.split("-");
+    return new Date(year, parseInt(month - 1));
+}
+
+const get_diff_in_months = (from, to) => {
+    const from_date = parse_date(from)
+    const to_date = parse_date(to)
+
+    let months = to_date.getFullYear() - from_date.getFullYear()
+    months *= 12;
+    months += to_date.getMonth();
+    months -= from_date.getMonth();
+    return ++months;
+}
+
+const get_months = (from, to) => {
+    const from_date = parse_date(from)
+    const to_date = parse_date(to)
+
+    const months = [];
+
+    const from_year = from_date.getFullYear();
+    const to_year = to_date.getFullYear()
+    const from_month = from_date.getMonth();
+    const to_month = to_date.getMonth();
+
+    for (let index = from_year; index <= to_year; index++) {
+        for (let jndex = (index === from_year ? from_month : 0); jndex <= (index === to_year ? to_month : 11); jndex++) {
+            months.push({ year: index, month: jndex })
+        }
+    }
+
+    return months;
+}
 
 export {
-    generate_data, random, numbers,
+    generate_data, random, numbers, get_diff_in_months, get_months, parse_date,
 }
